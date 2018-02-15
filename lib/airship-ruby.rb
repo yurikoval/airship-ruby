3469230ef347d5ea5609af491fd187718cff3113
George Li <glusa8@berkeley.edu>
2018-02-15 13:32:14 -0800

    Skeleton

--- lib/airship-ruby.rb
+++ lib/airship-ruby.rb
@@ -13,33 +13,73 @@ class Airship
     @gateStatsUploadBatchInterval = 60
 
     @gateStatsWatcher = nil
+    @gateStatsLastTaskScheduledTimestamp = Concurrent::AtomicFixnum.new(0)
 
     @gateStatsUploaderTasks = []
 
     @gateStatsBatch = []
 
-    @initLock = Concurrent::Semaphore.new(1)
+    @gateStatsBatchLock = Concurrent::Semaphore.new(1)
     @gateStatsWatcherLock = Concurrent::Semaphore.new(1)
     @gateStatsUploaderTasksLock = Concurrent::Semaphore.new(1)
   end
 
-  def init()
-    @initLock.acquire
-
+  def init
+    # Not thread safe yet
     if @gatingInfoDownloaderTask.nil?
-      @gatingInfoDownloaderTask = Concurrent::TimerTask.new(execution_interval: 60, timeout_interval: 10, run_now: true) do |task|
-        puts "Pulling data from server\n"
-      end
+      @gatingInfoDownloaderTask = self._createPoller
       @gatingInfoDownloaderTask.execute
     end
 
     if @gateStatsWatcher.nil?
-      @gateStatsWatcher = Concurrent::TimerTask.new(execution_interval: 60, timeout_interval: 10, run_now: true) do |task|
-        puts "Uploading data to server\n"
-      end
+      @gateStatsWatcher = self._createWatcher
       @gateStatsWatcher.execute
     end
+    # Thread safe after this point
+  end
 
-    @initLock.release
+  def _createPoller
+    Concurrent::TimerTask.new(execution_interval: 60, timeout_interval: 10, run_now: true) do |task|
+      puts "Pulling data from server\n"
+    end
+  end
+
+  def _createWatcher
+    Concurrent::TimerTask.new(execution_interval: 60, timeout_interval: 10, run_now: true) do |task|
+      now = Time.now.utc.to_i
+      if now - @gateStatsLastTaskScheduledTimestamp.value >= 60
+        processed = self._processBatch(0)
+        if processed
+          @gateStatsLastTaskScheduledTimestamp.value = now
+        end
+      end
+    end
+  end
+
+  def _processBatch(limit)
+    processed = false
+    @gateStatsBatchLock.acquire
+    if @gateStatsBatch.size > limit
+      # TODO: do actual processing
+      processed = true
+    end
+    @gateStatsBatchLock.release
+    processed
+  end
+
+  def _checkBatchSizeAndMaybeProcess
+    processed = self._processBatch(@maxGateStatsBatchSize - 1)
+    if processed
+      now = Time.now.utc.to_i
+      @gateStatsLastTaskScheduledTimestamp.value = now
+    end
+  end
+
+  def uploadStatsAsync(stats)
+    @gateStatsBatchLock.acquire
+    @gateStatsBatch.push(stats)
+    @gateStatsBatchLock.release
+
+    self._checkBatchSizeAndMaybeProcess
   end
 end
