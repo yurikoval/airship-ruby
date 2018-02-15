c71fdde1756f8d0eed4457124e1a2a111e9841ee
George Li <glusa8@berkeley.edu>
2018-02-15 12:06:30 -0800

    Rename

--- lib/airship-ruby.rb
+++ lib/airship-ruby.rb
@@ -5,20 +5,23 @@ require 'concurrent'
 class Airship
   def initialize(options)
     @gatingInfo = nil
-    @gatingInfoThread = nil
+    @gatingInfoTask = nil
 
     @gatingInfoMap = nil
 
     @maxGateStatsBatchSize = 500
     @gateStatsUploadBatchInterval = 60
 
-    @gateStatsUploadThread = nil
+    @gateStatsUploadTask = nil
     @gateStatsBatch = []
 
     @semaphore = Concurrent::Semaphore.new(1)
   end
 
   def init()
+    @gateInfoTask = Concurrent::TimerTask.new(execution_interval: 60, timeout_interval: 10, run_now: true) do |task|
 
+    end
+    @gateInfoTask.execute
   end
 end
